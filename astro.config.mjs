5e025447475a7b674b0ddfa5cd87b2fdf655d964
Utkarsh Mishra <umishra707@gmail.com>
2026-08-27 04:33:54 +0530

    Improved website responsiveness

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,9 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://hexboardgame.com',
   vite: {
     plugins: [tailwindcss()]
   }
 });
+
